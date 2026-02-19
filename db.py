e44c2cac9073469a1402740e85deabd953cfad08
bandarurishitha <bandarurishitha0407@gmail.com>
2026-02-19 20:43:55 +0530

    db updated

--- db.py
+++ db.py
@@ -1,16 +1,12 @@
-from dotenv import load_dotenv
-import os
 import mysql.connector
 
-# Load .env from the same directory as this file (or workspace)
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
 
 def get_conn():
     conn = mysql.connector.connect(
         host="localhost",
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        user="root",              
+        password="root@123",
+        database="incident_db"
     )
     return conn
+
